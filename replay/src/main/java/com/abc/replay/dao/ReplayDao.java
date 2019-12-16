0b75ddc429d279cc2c8bf490879085634188ae7b
rautni <ninad.raut@lamresearch.com>
2019-12-17 00:53:20 +0530

    fix for db connection issue

--- replay/src/main/java/com/abc/replay/dao/ReplayDao.java
+++ replay/src/main/java/com/abc/replay/dao/ReplayDao.java
@@ -164,6 +164,12 @@ public class ReplayDao {
         try
         {
             Configuration conf = new Configuration();
+            conf.set("fs.hdfs.impl",
+                    org.apache.hadoop.hdfs.DistributedFileSystem.class.getName()
+            );
+            conf.set("fs.file.impl",
+                    org.apache.hadoop.fs.LocalFileSystem.class.getName()
+            );
             conf.set("hadoop.security.authentication", "Kerberos");
             UserGroupInformation.setConfiguration(conf);
             UserGroupInformation ugi = UserGroupInformation
